1ed93a1a0646a4651163e22a197fb48bd47a401a
Zheng-Jiabao <48375398+Zheng-Jiabao@users.noreply.github.com>
2019-04-04 18:06:03 -0500

    Update main.js

--- En-Past tence/main.js
+++ En-Past tence/main.js
@@ -5,12 +5,21 @@ document.getElementById("quastion").innerHTML = quastion
 document.getElementById("lebal2").innerHTML = lableII
 document.getElementById("title").innerHTML = "En:" + lableII
 document.getElementById("UsersAn").value=""
+function turnlocal(loadbool){
+if(loadbool==1){
+window.location.reload()
+}else{
+window.location.href=window.location.href+Answersmain[TheQuastionNumberForArray]
+}
+}
 function showed(){
 	var UsersBack=document.getElementById("UsersAn").value
 	if(UsersBack==Answersmain[TheQuastionNumberForArray]){
 		document.getElementById("Istrue").innerHTML="RIGHT"
+setTimeout(1000,turnlocal(1))
 	}else{
 		document.getElementById("Istrue").innerHTML="WRONG"
 		document.getElementById("Answer").innerHTML=Answersmain[TheQuastionNumberForArray]
+		setTimeout(1000,turnlocal(0))
 		}
 }
