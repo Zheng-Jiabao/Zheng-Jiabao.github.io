3b44db8b99a7282c2d1f92c116de7de035b93bab
Zheng-Jiabao <48375398+Zheng-Jiabao@users.noreply.github.com>
2019-05-02 20:18:44 +0800

    Change

---
+++ Score/Data.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+var TasksWeA=["First Month Task","Mid-Team Task"];
+var ScoreData=[[108.5,145,134,72,88,43,38,0,0],[10]];
+var FullScore=[150,150,150,100,100,50,50,5,5];
--- Score/score.js
+++ Score/score.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+$("#BARorLINE").hide(0);
+$("#BarChooseWhichTask").hide(0);
+$("#BarMain").hide(0);
+
+$("#BARorLINE").show(0);
+$("#chooseBAR").click(function(){
+	 $("#BarChooseWhichTask").show(0);
+	 $("#BARorLINE").hide(0);
+	 $("#howmanytasks").text(TasksWeA.length);
+	 for (var i=0; i<TasksWeA.length; i++){
+		 $("#BarChooseWhichTaskList").html($("#BarChooseWhichTaskList").html()+"<li>"+TasksWeA[i]+"</li>");
+		 $("ol li:eq("+i+")").click(
+			function(){
+				BarMaker(i);
+				}
+			);
+		 }
+	 })
+function BarMaker(whichtast){
+	$("#BarChooseWhichTask").hide(0);
+	$("#BarMain").show(0);
+	$("#TaskName").text(TasksWeA[whichtast])
+	}
