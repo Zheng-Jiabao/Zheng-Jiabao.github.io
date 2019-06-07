75a8a63ec8e741b0c201c38092a680cf3e0092d6
Zheng-Jiabao <48375398+Zheng-Jiabao@users.noreply.github.com>
2019-06-07 11:05:43 +0800

    update

--- Score/score.js
+++ Score/score.js
@@ -5,22 +5,15 @@ var app=new Vue({
 		willshow:{
 			BARorLINE:true,
 			BarChooseWhichTask:false,
-			BarMain:false
 		},
 		TaskNumber:0,
 		TaskNames:[
 			{Name:"First Month Task"},
 			{Name:"Mid-Team Task"}
-		],
-		getScore:[
-				  
-				  ]
+		]
 	},
 	methods:{
-		toBarMain:function(whichTask){
-			this.willshow.BarMain=true;
-			this.willshow.BarChooseWhichTask=false;
-			}
+
 	}
 })
 app.TaskNumber=app.TaskNames.length
@@ -29,15 +22,7 @@ app.willshow.BarChooseWhichTask=false;
 app.willshow.BarMain=false;
 $("#chooseBAR").click(function(){
 			app.willshow.BARorLINE=false;
-			app.willshow.BarChooseWhichTask=true;	
-							   })
+			app.willshow.BarChooseWhichTask=true;
+			
+})
 
-
-
-
-
-function BarMaker(whichtast){
-	$("#BarChooseWhichTask").hide(0);
-	$("#BarMain").show(0);
-	$("#TaskName").text(TasksWeA[whichtast])
-	}
